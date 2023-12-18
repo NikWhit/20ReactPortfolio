2f1b324ebe93fe03ddb78f532a0737ca171844a2
NikWhit <135679785+NikWhit@users.noreply.github.com>
2023-12-17 23:26:56 -0700

    added header and footer components and some nav utility

--- src/components/NavigationComponent.jsx
+++ src/components/NavigationComponent.jsx
@@ -1,6 +1,6 @@
 export default function Nav({ links }) {
     return (
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <ul className="link-text">
         {links.map((link) => link)}
       </ul>
     );
