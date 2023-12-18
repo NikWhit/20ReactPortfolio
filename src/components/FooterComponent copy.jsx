551ed3fcadaa3c8b4098820bf04ad64342fa80d7
NikWhit <135679785+NikWhit@users.noreply.github.com>
2023-12-18 00:45:11 -0700

    Delete src/components directory

--- src/components/FooterComponent copy.jsx
+++
@@ -1,9 +0,0 @@
-function Footer(){
-    return(
-        <footer>
-            <span className="footer">Nick Whitaker</span>
-            <span className="footer2">2023</span>
-        </footer>
-    )
-}
-export default Footer
--- src/components/NavigationComponent copy.jsx
+++
@@ -1,7 +0,0 @@
-export default function Nav({ links }) {
-    return (
-        <ul className="link-text">
-        {links.map((link) => link)}
-      </ul>
-    );
-}
